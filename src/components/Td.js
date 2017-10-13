5783a6137d515b06081b01e7aa8a3bf2bedef6c4
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-10-13 11:08:09 +0200

    Td.js for loop modification, to be handled in map

--- src/components/Td.js
+++ src/components/Td.js
@@ -22,25 +22,23 @@ class Td extends Component {
   render() {
 
     let events = this.state.events;
-
-    let mappedEvents = events.map((event) => {
-      return <Event
-              key={event.id}
-              description={`${event.patient.salutation} ${event.patient.firstname}`}
-              start={event.start_time}
-              end={event.end_time}
-              day={event.day}
-              sendData={(data) => this.handleSendData(data)}
-              />
-    })
-
     let currentEvent;
 
-    for (var i = 0; i < mappedEvents.length; i++) {
-      if (mappedEvents[i].props.start === this.props.hour && mappedEvents[i].props.day === this.props.day) {
-        currentEvent = mappedEvents[i]
+    events.map((event) => {
+      if (event.start_time === this.props.hour && event.day === this.props.day) {
+
+         currentEvent =
+              <Event
+                key={event.id}
+                description={`${event.patient.salutation} ${event.patient.firstname}`}
+                start={event.start_time}
+                end={event.end_time}
+                day={event.day}
+                sendData={(data) => this.handleSendData(data)}
+              />
       }
-    }
+      return currentEvent;
+    })
 
     return (
       <td day={this.props.day} hour={this.props.hour}>{currentEvent}</td>
